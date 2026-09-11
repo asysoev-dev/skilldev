7861abc79954db63f6af60062814944b57605ee0
Alexey Sysoev <blash210787@gmail.com>
2026-09-11 15:23:07 +0500

    chore(ui): add barrel export for shared UI components

---
+++ frontend/src/shared/ui/index.ts
@@ -0,0 +1,11 @@
+export { default as Button } from './Button/Button.vue';
+export { default as Input } from './Input/Input.vue';
+export { default as Textarea } from './Textarea/Textarea.vue';
+export { default as Checkbox } from './Checkbox/Checkbox.vue';
+export { default as Radio } from './Radio/Radio.vue';
+export { default as Switch } from './Switch/Switch.vue';
+export { default as GlassCard } from './GlassCard/GlassCard.vue';
+export { default as Tag } from './Tag/Tag.vue';
+
+export type { ButtonVariant, ButtonSize, ButtonType } from './Button/Button.vue';
+export type { TagVariant, TagSize } from './Tag/Tag.vue';
