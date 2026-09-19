bdcab38cb740e24264b978b7b1db9f2568d4dbfc
Alexey Sysoev <blash210787@gmail.com>
2026-09-19 15:10:02 +0500

    chore(build): enable hydration mismatch details for debugging

--- frontend/webpack/webpack.base.js
+++ frontend/webpack/webpack.base.js
@@ -55,7 +55,7 @@ module.exports = {
         new webpack.DefinePlugin({
             __VUE_OPTIONS_API__: JSON.stringify(true),
             __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
         }),
     ],
 };
